0803b4bb50b02a4fc2bfacc9ac83085f8f36c811
waibaBikash <kavrelibikash@gmail.com>
2024-12-08 23:12:49 +1100

    commit change

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -13,10 +13,18 @@ const Header = () => {
        </div>
          <div className='text-center my-8'>
             <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
-            <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aliquam dignissimos beatae, fugiat at veritatis!</p>
-             <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black '>
+            <p 
+            className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Illo aliquam dignissimos beatae, fugiat at veritatis!
+              </p>
+             <form 
+             className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black '>
              <input type="email" placeholder='Enter your email' className='pl-4 outline-none' />
-             <button className='border-1 border-black py-4 px-4 sm:px-8 active:border-t-gray-600 active:text-white '>Subscribe</button>
+             <button
+              className='border-1 border-black py-4 px-4 sm:px-8 active:border-t-gray-600 active:text-white '>
+                Subscribe
+                </button>
              </form>
          </div>
     </div>
