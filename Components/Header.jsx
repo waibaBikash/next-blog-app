76382c1183a39cb3a333fd3eb8c15dda3ae7f404
waibaBikash <kavrelibikash@gmail.com>
2024-12-08 23:11:48 +1100

    initial commit

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
     <div className='py-5 px-5 md:px-12 lg:px-28'>
        <div className='flex justify-between items-center'>
            <Image src={assets.logo} width={180 } alt='' className='w-[130px] sm:w-auto'/>
-            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/></button>
+            <button 
+            className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/>
+            </button>
        </div>
          <div className='text-center my-8'>
             <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
