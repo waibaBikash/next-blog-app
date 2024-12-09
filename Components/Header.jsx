7c9648e0d662915c378d81a0489f4e4b88f844d3
waibaBikash <kavrelibikash@gmail.com>
2024-12-09 22:17:28 +1100

    complete header section

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
        <div className='flex justify-between items-center'>
            <Image src={assets.logo} width={180 } alt='' className='w-[130px] sm:w-auto'/>
             <button 
-            className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/>
+            className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow} alt=''/>
             </button>
        </div>
          <div className='text-center my-8'>
@@ -19,10 +19,10 @@ const Header = () => {
               Illo aliquam dignissimos beatae, fugiat at veritatis!
               </p>
              <form 
-             className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black '>
+             className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'>
              <input type="email" placeholder='Enter your email' className='pl-4 outline-none' />
-             <button
-              className='border-1 border-black py-4 px-4 sm:px-8 active:border-t-gray-600 active:text-white '>
+             <button type='submit'
+              className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white '>
                 Subscribe
                 </button>
              </form>
