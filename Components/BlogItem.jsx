2451258d0d0661dfa278218f8ae13fc4ec6fbbed
waibaBikash <kavrelibikash@gmail.com>
2024-12-11 21:47:31 +1100

    save change

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -3,7 +3,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const BlogItem = ({title,description,category,image,id}) => {
+const BlogItem = ({title,
+  description,
+  category,
+  image,
+  id}) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover: shadow-[-7px_7px_0px_#000000]'>
       <Link href={`/blogs/${id}`}>
