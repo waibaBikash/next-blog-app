3c9d3291e679a33f099860b14b934d98e4eb552b
waibaBikash <kavrelibikash@gmail.com>
2024-12-11 21:48:58 +1100

    commit change

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -18,8 +18,14 @@ const BlogItem = ({title,
         {category}
        </p>
        <div className='p-5'>
- <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900 '>{title}</h5>
- <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
+      <h5 
+       className='mb-2 text-lg font-medium tracking-tight text-gray-900 '>
+      {title}
+      </h5>
+       <p 
+         className='mb-3 text-sm tracking-tight text-gray-700'>
+          {description}
+          </p>
           <Link href={`/blogs/${id}`} 
           className='inline-flex items-center py-2 font-semibold text-center'>
             Read more
