a04036c3d5f0e8f04b59fbeb5dfc68c36d7fa465
waibaBikash <kavrelibikash@gmail.com>
2024-12-12 22:38:51 +1100

    save change

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,7 +1,7 @@
-import { assets } from '@/assets/assets'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { assets } from '@/assets/assets';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const BlogItem = ({title,
   description,
