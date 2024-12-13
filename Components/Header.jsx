40b767fd5f990ce11ae4d1f14ddf4a46e9f5531a
waibaBikash <kavrelibikash@gmail.com>
2024-12-13 23:26:27 +1100

    changes made

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='py-5 px-5 md:px-12 lg:px-28'>
-       <div className='flex justify-between items-center'>
+    <div 
+      className='py-5 px-5 md:px-12 lg:px-28'>
+       <div
+         className='flex justify-between items-center'>
            <Image src={assets.logo} width={180 } alt='' className='w-[130px] sm:w-auto'/>
             <button 
             className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow} alt=''/>
