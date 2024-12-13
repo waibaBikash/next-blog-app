ee343b422b745be9af14abedfd2f5b6e519c1a02
waibaBikash <kavrelibikash@gmail.com>
2024-12-13 23:25:55 +1100

    save change

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
+    <div 
+    className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
         <Image src={ assets.logo_light } alt='' width={120}/>
         <p className='text-sm text-white'>All rihgt reserved. Copyright @blogger</p>
          <div className="flex">
