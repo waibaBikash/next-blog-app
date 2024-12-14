6fa20d6d08a8f4961d7b9850295205d1bbcdc2ec
waibaBikash <kavrelibikash@gmail.com>
2024-12-14 22:57:43 +1100

    save change

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -9,7 +9,8 @@ const BlogItem = ({title,
   image,
   id}) => {
   return (
-    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover: shadow-[-7px_7px_0px_#000000]'>
+    <div 
+     className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover: shadow-[-7px_7px_0px_#000000]'>
       <Link href={`/blogs/${id}`}>
       <Image src={image} alt='' width={400} height={400} className='border-b border-black'/>
       </Link>
