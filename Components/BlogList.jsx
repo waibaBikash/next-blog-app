b49a98a98c711e6f2368c13f084f801e623ec2b4
waibaBikash <kavrelibikash@gmail.com>
2024-12-14 22:59:34 +1100

    save change

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -11,7 +11,8 @@ const BlogList = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-6 my-10'>
+      <div 
+      className='flex justify-center gap-6 my-10'>
           <button 
           onClick={()=>setMenu('All')}
            className={ menu==="All" ? 'bg-black text-white py-1 px-4 rounded-sm' : "" }>
@@ -27,8 +28,9 @@ const BlogList = () => {
             Startup</button>
           <button 
           onClick={()=>setMenu('Lifestyle')} 
-          className={ menu==="Lifestyle" ? 'bg-black text-white py-1 px-4 rounded-sm' : "" }>
-            Lifestyle</button>
+          className={ menu==="Lifestyle" ? 'bg-black text-white   py-1 px-4 rounded-sm' : "" }>
+            Lifestyle
+            </button>
       </div>
        <div 
        className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24 '>
