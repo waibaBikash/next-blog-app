a42ff782098391771bf2afa814933abd843532b2
waibaBikash <kavrelibikash@gmail.com>
2024-12-14 22:58:13 +1100

    commit change

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -12,7 +12,11 @@ const BlogItem = ({title,
     <div 
      className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover: shadow-[-7px_7px_0px_#000000]'>
       <Link href={`/blogs/${id}`}>
-      <Image src={image} alt='' width={400} height={400} className='border-b border-black'/>
+      <Image
+       src={image} 
+       alt='' width={400} 
+       height={400}
+        className='border-b border-black'/>
       </Link>
       <p
        className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>
