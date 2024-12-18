39ce79645fec3d3b286ab6bc66e95982ee83f2a1
waibaBikash <kavrelibikash@gmail.com>
2024-12-18 22:31:56 +1100

    changes made

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -7,7 +7,10 @@ const Footer = () => {
     <div 
     className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
         <Image src={ assets.logo_light } alt='' width={120}/>
-        <p className='text-sm text-white'>All rihgt reserved. Copyright @blogger</p>
+        <p 
+         className='text-sm text-white'>
+          All rihgt reserved. Copyright @blogger
+          </p>
          <div className="flex">
            <Image src={ assets.facebook_icon} alt=''/>
            <Image src={ assets.twitter_icon} alt=''/>
