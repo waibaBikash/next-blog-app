7d97fdebfa0e6503705797d3f1486e29969d61b8
waibaBikash <kavrelibikash@gmail.com>
2024-12-21 22:55:13 +1100

    changes made

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
   return (
     <div 
     className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
-        <Image src={ assets.logo_light } alt='' width={120}/>
+        <Image 
+         src={ assets.logo_light }
+          alt='' 
+          width={120}
+          />
         <p 
          className='text-sm text-white'>
           All rihgt reserved. Copyright @blogger
